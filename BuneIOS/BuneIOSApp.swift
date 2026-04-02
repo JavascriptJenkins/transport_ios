5ab7c3741a66ab33db4270d05844b8383375b5a8
joseande <joe111111@gmail.com>
2026-04-02 17:49:27 -0400

    Initial Commit

---
+++ BuneIOS/BuneIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuneIOSApp.swift
+//  BuneIOS
+//
+//  Created by techvvs001 on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct BuneIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuneIOSTests/BuneIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuneIOSTests.swift
+//  BuneIOSTests
+//
+//  Created by techvvs001 on 4/2/26.
+//
+
+import Testing
+@testable import BuneIOS
+
+struct BuneIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
